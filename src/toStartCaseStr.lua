9d084cc9677cfb1bea018f0ae2ced4a8ae28f815
Marin Minnerly <me@vocksel.com>
2023-12-17 14:30:33 -0800

    Manually convert toStartCaseStr

--- src/toStartCaseStr.lua
+++ src/toStartCaseStr.lua
@@ -1,30 +1,60 @@
 -- ROBLOX upstream: https://github.com/ComponentDriven/csf/blob/v0.1.2-next.0/src/toStartCaseStr.ts
 local Packages = script:FindFirstAncestor("Packages")
 
-local RegExp = require(Packages.RegExp)
+local LuauPolyfill = require(Packages.LuauPolyfill)
+
+type ReplacementFn = string | (...string) -> string
+
+-- ROBLOX deviation START: Added a `replace` polyfill since LuauPolyfill doesn't have one
+local function replace(str: string, pattern: string, replacement: ReplacementFn)
+	local matches = {}
+	for match in str:gmatch(pattern) do
+		table.insert(matches, match)
+	end
+
+	for _, match in matches do
+		if typeof(replacement) == "string" then
+			str = str:gsub(match, replacement)
+		else
+			str = str:gsub(match, replacement(table.unpack(matches)))
+		end
+	end
+
+	return str
+end
+-- ROBLOX deviation END
 
 local exports = {}
 local function toStartCaseStr(str: string)
-	return str:replace(RegExp("_", "g"), " ")
-		:replace(RegExp("-", "g"), " ")
-		:replace(RegExp(".", "g"), " ")
-		:replace(RegExp("([^\n])([A-Z])([a-z])", "g"), function(_, m1, m2, m3)
-			return ("%s %s%s"):format(tostring(m1), tostring(m2), tostring(m3))
-		end)
-		:replace(RegExp("([a-z])([A-Z])", "g"), function(_, m1, m2)
-			return ("%s %s"):format(tostring(m1), tostring(m2))
-		end)
-		:replace(RegExp("([a-z])([0-9])", "gi"), function(_, m1, m2)
-			return ("%s %s"):format(tostring(m1), tostring(m2))
-		end)
-		:replace(RegExp("([0-9])([a-z])", "gi"), function(_, m1, m2)
-			return ("%s %s"):format(tostring(m1), tostring(m2))
-		end)
-		:replace(RegExp("(s|^)(w)", "g"), function(_, m1, m2)
-			return ("%s%s"):format(tostring(m1), tostring(m2:toUpperCase()))
-		end)
-		:replace(RegExp(" +", "g"), " ")
-		:trim()
+	-- ROBLOX deviation START: Manually translated regex to Luau string patterns
+	str = str:gsub("_", " "):gsub("-", " "):gsub("%.", " ")
+
+	str = replace(str, "([^\n])([A-Z])([a-z])", function(m1, m2, m3)
+		return `{m1} {m2}{m3}`
+	end)
+
+	str = replace(str, "([a-z])([A-Z])", function(m1, m2)
+		return `{m1} {m2}`
+	end)
+
+	str = replace(str, "([a-z])([0-9])", function(m1, m2)
+		return `{m1} {m2}`
+	end)
+
+	str = replace(str, "([0-9])([a-z])", function(m1, m2)
+		return `{m1} {m2}`
+	end)
+
+	str = replace(str, "(%s|^)(%w)", function(m1, m2)
+		return `{m1} {m2:upper()}`
+	end)
+
+	str = str:gsub(" +", " ")
+
+	str = LuauPolyfill.String.trim(str)
+	-- ROBLOX deviation END
+
+	return str
 end
 exports.toStartCaseStr = toStartCaseStr
 return exports
