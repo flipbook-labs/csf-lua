f8d6abe59bf40a65afbdb916ae62c3da74ce21d3
Marin Minnerly <me@vocksel.com>
2023-12-23 12:56:54 -0800

    Add a description to example

--- example/init.server.lua
+++ example/init.server.lua
@@ -1,3 +1,6 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local CSF = require(ReplicatedStorage.Packages.CSF)
+
+-- Type `CSF.` to get autocompletes. Use `type Foo = CSF.` to autocomplete
+-- exported types
